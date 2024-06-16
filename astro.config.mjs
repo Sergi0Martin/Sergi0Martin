65ce58ce01a3377f39aa79b1e4b0f5a1a4c7fcd9
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2024-06-16 15:20:33 +0200

    Fix css error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
-
+import tailwind from '@astrojs/tailwind'
+import tailwindcssNesting from 'tailwindcss/nesting'
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://Sergi0Martin.github.io'
+  integrations: [
+    tailwind({ applyBaseStyles: true }),
+    robotsTxt()
+  ],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting()]
+      }
+    }
+  },
+  site: 'https://Sergi0Martin.github.io',
 })
